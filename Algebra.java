bd879e9bb402aaf338208fde08512e15020448bc
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-23 18:46:15 +0200

    done

--- Algebra.java
+++ Algebra.java
@@ -6,43 +6,45 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3 = 5
-		System.out.println(plus(2,-3));     // 2 + (-3) = -1
-		System.out.println(plus(-2,3));     // (-2) + 3 = 1
-		System.out.println(plus(-2,-3));       // (-2) + (-3) = -5
-	    System.out.println(minus(7,2));  // 7 - 2 = 5
-		System.out.println(minus(7,-2));    // 7 - (-2) = 9
-		System.out.println(minus(-7,2));    // (-7) - 2 = -9
-		System.out.println(minus(-7,-2));      // (-7) - (-2) = -5
- 		System.out.println(times(3,4));  // 3 * 4 = 12
-		System.out.println(times(-3,4));   // (-3) * 4 = -12
-		System.out.println(times(3,-4));   // 3 * (-4) = -12
-		System.out.println(times(-3,-4));     // (-3) * (-4) = 12
-   		System.out.println(plus(5,times(4,2)));  // 5 + 4 * 2
-		System.out.println(plus(0,times(-9,2)));  // 0 + -9 * 2
-		System.out.println(plus(-1,times(4,-4)));  // -1 + 4 * -4
-   		System.out.println(pow(5,3));      // 125
-   		System.out.println(pow(3,5));      // 243
-		System.out.println(pow(3,0));         // 1
-		System.out.println(pow(-3,5));        // -243
-	    System.out.println(pow(0,5));        // 0
-	    System.out.println(pow(1,5));        // 1
-   		System.out.println(div(12,3));   // 4    
-   		System.out.println(div(5,5));    // 1  
-   		System.out.println(div(25,7));   // 3
-	    System.out.println(div(-6,2));      //-3
-		System.out.println(div(6,-2));      //-3
-		System.out.println(div(0,7));    // 0
-		System.out.println(div(3,1));    // 3
-   		System.out.println(mod(6,4));       // 25 % 7
-		System.out.println(mod(6,3));       // 25 % 7
-		System.out.println(mod(34,8));       // 2
-   		System.out.println(mod(120,6));      // 120 % 6   
-		System.out.println(mod(-8,6));
-		System.out.println(mod(6,-2)); 
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
+	    //System.out.println(plus(2,3));   // 2 + 3 = 5
+		//System.out.println(plus(2,-3));     // 2 + (-3) = -1
+		//System.out.println(plus(-2,3));     // (-2) + 3 = 1
+		//System.out.println(plus(-2,-3));       // (-2) + (-3) = -5
+	    //System.out.println(minus(7,2));  // 7 - 2 = 5
+		//System.out.println(minus(7,-2));    // 7 - (-2) = 9
+		//System.out.println(minus(-7,2));    // (-7) - 2 = -9
+		//System.out.println(minus(-7,-2));      // (-7) - (-2) = -5
+ 		//System.out.println(times(3,4));  // 3 * 4 = 12
+		//System.out.println(times(-3,4));   // (-3) * 4 = -12
+		//System.out.println(times(3,-4));   // 3 * (-4) = -12
+		//System.out.println(times(-3,-4));     // (-3) * (-4) = 12
+   		//System.out.println(plus(5,times(4,2)));  // 5 + 4 * 2
+		//System.out.println(plus(0,times(-9,2)));  // 0 + -9 * 2
+		//System.out.println(plus(-1,times(4,-4)));  // -1 + 4 * -4
+   		//System.out.println(pow(5,3));      // 125
+   		//System.out.println(pow(3,5));      // 243
+		//System.out.println(pow(3,0));         // 1
+		//System.out.println(pow(-3,5));        // -243
+	    //System.out.println(pow(0,5));        // 0
+	    //System.out.println(pow(1,5));        // 1
+   		//System.out.println(div(12,3));   // 4    
+   		//System.out.println(div(-36,-6));    // 1  
+   		//System.out.println(div(25,7));   // 3
+	    //System.out.println(div(-6,2));      //-3
+		//System.out.println(div(6,-2));      //-3
+		//System.out.println(div(0,7));    // 0
+		//System.out.println(div(3,1));    // 3
+   		//System.out.println(mod(6,4));       // 25 % 7
+		//System.out.println(mod(6,3));       // 25 % 7
+		//System.out.println(mod(34,8));       // 2
+   		//System.out.println(mod(120,6));      // 120 % 6   
+		//System.out.println(mod(-8,6));
+		//System.out.println(mod(6,-2)); 
+   		//System.out.println(sqrt(0));
+		//System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
+		System.out.println(sqrt(9));
+		System.out.println(sqrt(81));
 	}  
 
 	// Returns x1 + x2
@@ -133,7 +135,16 @@ public class Algebra {
 		return x1;
 		int q = x1;
 		int count = 0;
-		if((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) { 
+		if(x1 < 0 && x2 < 0){
+			x2 = Math.abs(x2);
+			x1 = Math.abs(x1);
+			q = x1;
+			while(q >= x2) {
+				q = minus(q, x2);
+				count++;
+			}
+			return count;
+		} else if((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) { 
 			x2 = Math.abs(x2);
 			x1 = Math.abs(x1);
 			q = x1;
@@ -166,9 +177,11 @@ public class Algebra {
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
 		int root = 0;
-		while(pow(root, 2) < x){
+		while(pow(root, 2) < x) {
 			root++;
 		}
+		if(times(root, root) > x)
+		root--;
 		return root;
 	}	  	  
 }
