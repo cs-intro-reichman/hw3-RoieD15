f18c7cd07d5a6c03512553c7319ad0910d34577b
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-25 11:42:42 +0200

    done ex 2

--- LoanCalc.java
+++ LoanCalc.java
@@ -13,7 +13,6 @@ public class LoanCalc {
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
-		System.out.println(endBalance(100000, 5, 10, 12333));
 		// Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
